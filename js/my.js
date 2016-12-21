e4c4c584c8c25a820a6e6d4021563a1ac330840d
Mark Richman <mark@markrichman.com>
2016-12-21 20:14:56 +0000

    rebuilding site Wed Dec 21 20:14:56 UTC 2016

--- js/my.js
+++ js/my.js
@@ -1,21 +1,23 @@
-$("#contact-form").validate({
-    submitHandler: function(form) {
-        $.ajax({
-            url: "//formspree.io/mark@markrichman.com",
-            method: "POST",
-            data: {
-                name: $(form).find("input[name='name']").val(),
-                _replyto: $(form).find("input[name='_replyto']").val(),
-                message: $(form).find("textarea[name='message']").val()
-            },
-            dataType: "json",
-            success: function() {
-                $("#submit-success").fadeIn();
-                $("#contact-form").fadeOut();
-            },
-            error: function() {
-                $("#submit-errors").fadeIn();
-            }
-        });
-    }
-});
+if (typeof jQuery != 'undefined') {     
+    $("#contact-form").validate({
+        submitHandler: function(form) {
+            $.ajax({
+                url: "//formspree.io/mark@markrichman.com",
+                method: "POST",
+                data: {
+                    name: $(form).find("input[name='name']").val(),
+                    _replyto: $(form).find("input[name='_replyto']").val(),
+                    message: $(form).find("textarea[name='message']").val()
+                },
+                dataType: "json",
+                success: function() {
+                    $("#submit-success").fadeIn();
+                    $("#contact-form").fadeOut();
+                },
+                error: function() {
+                    $("#submit-errors").fadeIn();
+                }
+            });
+        }
+    });
+}
